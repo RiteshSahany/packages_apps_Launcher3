5f855af1b5439195b9fb1fb9e78020191dcb6394
Ido Ben-Hur <idoybh2@gmail.com>
2024-07-22 15:41:38 +0300

    Launcher3: Always hide the keyboard when the drawer is minimized
    if you manually minimize it with the keyboard open it just stays there

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -157,6 +157,7 @@ import android.view.WindowInsetsAnimation;
 import android.view.WindowManager.LayoutParams;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.animation.OvershootInterpolator;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 import android.window.BackEvent;
 import android.window.OnBackAnimationCallback;
@@ -434,6 +435,8 @@ public class Launcher extends StatefulActivity<LauncherState>
                 public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {}
             };
 
+    private InputMethodManager mInputMethodManager;
+
     public static Launcher getLauncher(Context context) {
         return fromContext(context);
     }
@@ -623,6 +626,9 @@ public class Launcher extends StatefulActivity<LauncherState>
         TestEventEmitter.sendEvent(TestEvent.LAUNCHER_ON_CREATE);
 
         mSharedPrefs.registerOnSharedPreferenceChangeListener(mSharedPrefListener);
+
+        mInputMethodManager = (InputMethodManager) mWorkspace.getContext().getSystemService(
+                Context.INPUT_METHOD_SERVICE);
     }
 
     protected ModelCallbacks createModelCallbacks() {
@@ -2816,7 +2822,11 @@ public class Launcher extends StatefulActivity<LauncherState>
      * @param progress Transition progress from 0 to 1; where 0 => home and 1 => all apps.
      */
     public void onAllAppsTransition(float progress) {
-        // No-Op
+        if (progress > 0) return;
+        if (mAppsView == null) return;
+        if (mInputMethodManager == null) return;
+        // make sure the keyboard is hidden when the AllApps view is closed
+        mInputMethodManager.hideSoftInputFromWindow(mAppsView.getWindowToken(), 0);
     }
 
     /**
